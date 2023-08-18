603276c0d7185b55a5b0d213a0f904313bddc237
triduc98 <106300987+triduc98@users.noreply.github.com>
2023-08-18 20:17:28 +0700

    93. Cài đặt và cấu hình Prettier

--- src/App.js
+++ src/App.js
@@ -5,19 +5,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Button/>
-
+      <Button />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
