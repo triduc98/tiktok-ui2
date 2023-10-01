317bf41b4638950be7c8ea6f476a4543268d41bb
Duc <nguyentr.duc92@gmail.com>
2023-10-01 19:18:17 +0700

    96.1 UI phần Header #1 --- logo tiktok svg:

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,12 +1,18 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import images from '~/assets/images';
 
 const cx = classNames.bind(styles);
+console.log(images.logo);
 
 function Header() {
   return (
     <header className={cx('wrapper')}>
-      <div className={cx('inner')}></div>
+      <div className={cx('inner')}>
+        <div className={cx('logo')}>
+          <img src={images.logo} alt="Tiktok" />
+        </div>
+      </div>
     </header>
   );
 }
